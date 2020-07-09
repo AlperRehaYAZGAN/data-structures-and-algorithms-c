26a1545c71f52d6c41ab6d4dedab293f769e16d4
AlperRehaYAZGAN <43668181+AlperRehaYAZGAN@users.noreply.github.com>
2020-07-09 20:59:06 +0300

    normal linked list added to linked list section

---
+++ linked-lists/linked-list-normal/cmake-build-debug/CMakeFiles/linked_list_normal.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/linked_list_normal.dir/main.c.obj"
+  "linked_list_normal.pdb"
+  "linked_list_normal.exe"
+  "linked_list_normal.exe.manifest"
+  "liblinked_list_normal.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/linked_list_normal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
